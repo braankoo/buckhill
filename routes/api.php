1d3e38696e20589f8606cc746e2f15a21774c65a
Branko Dragovic <braankoo@gmail.com>
2023-03-30 13:13:31 +0200

    Added create user route

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('admin')->group(function () {
-
         Route::post('create', [RegisterController::class, 'create'])->name('create');
         Route::post('login', [LoginController::class, 'login'])->name('login');
         Route::get('logout', [LoginController::class, 'logout'])->name('logout');
@@ -40,4 +39,8 @@ Route::prefix('v1')->group(function () {
                 ->name('delete');
         });
     })->name('admin.');
+
+    Route::prefix('user')->group(function () {
+        Route::post('create', [\App\Http\Controllers\User\RegisterController::class, 'create']);
+    });
 });
