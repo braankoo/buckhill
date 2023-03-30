5418153304de2edd369f694560c524e5e738e85a
Branko Dragovic <braankoo@gmail.com>
2023-03-30 13:05:40 +0200

    Register user controller

---
+++ app/Http/Controllers/User/RegisterController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Facades\Jwt;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\CreateRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Services\RegisterUser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use OpenApi\Annotations as OA;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+use Throwable;
+
+class RegisterController extends Controller
+{
+    /**
+     * @OA\Post(
+     *     path="/api/v1/user/create",
+     *     tags={"User"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="first_name",
+     *                     type="string",
+     *                     description="User First Name",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="last_name",
+     *                     type="string",
+     *                     description="User First Name",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     description="User email",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     description="User password",
+     *                 ),
+     *                @OA\Property(
+     *                     property="avatar",
+     *                     type="string",
+     *                     description="User password",
+     *                 ),
+     *               @OA\Property(
+     *                     property="address",
+     *                     type="string",
+     *                     description="User address",
+     *                 ),
+     *               @OA\Property(
+     *                     property="phone_number",
+     *                     type="string",
+     *                     description="User phone number",
+     *                 ),
+     *               @OA\Property(
+     *                     property="is_marketing",
+     *                     description="User marketing preferences",
+     *                     type="string",
+     *                     enum={"0", "1"},
+     *                 ),
+     *                 required={"first_name", "last_name", "email","password","address","phone_number"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
+     *     )
+     * )
+     *
+     * @throws Throwable
+     */
+    public function create(CreateRequest $request)
+    {
+        $registerService = app(RegisterUser::class);
+        $user = $registerService->create($request, UserResource::class, 0);
+        if (!$user) {
+            return Response::api(HttpResponse::HTTP_INTERNAL_SERVER_ERROR, 0, []);
+        }
+        return Response::api(HttpResponse::HTTP_CREATED, 1, $user);
+    }
+}
