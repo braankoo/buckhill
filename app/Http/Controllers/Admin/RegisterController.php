5c34fba0fae13c57eaa7ba458bbb0c4bf3b4bcce
Branko Dragovic <braankoo@gmail.com>
2023-03-30 11:09:39 +0200

    Register Admin Controller

--- app/Http/Controllers/Admin/RegisterController.php
+++ app/Http/Controllers/Admin/RegisterController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-
+use App\Facades\Jwt;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CreateRequest;
-
+use App\Http\Resources\AdminResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
-
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 /**
  *
@@ -17,6 +22,29 @@ final class RegisterController extends Controller
 
     public function create(CreateRequest $request): JsonResponse
     {
+        $attributes = $request->safe()->merge(['is_admin' => true])->all();
+        $attributes['password'] = Hash::make($attributes['password']);
+        try {
+            DB::beginTransaction();
 
+            $user = User::create($attributes);
+            $token = Jwt::provideToken($user);
+
+            $user->tokens()->create([
+                'unique_id' => $token->claims()->get('jti'),
+                'token_title' => 'access',
+                'expires_at' => $token->claims()->get('exp'),
+            ]);
+
+            DB::commit();
+
+            $resource = (new AdminResource($user));
+            $resource->additional(['token' => $token->toString()]);
+
+            return Response::api(HttpResponse::HTTP_CREATED, 1, $resource);
+        } catch (\Exception $e) {
+            Log::debug('Error while creating new admin user', [$e->getMessage(), $e->getTrace()]);
+            return Response::api(HttpResponse::HTTP_INTERNAL_SERVER_ERROR, 0, []);
+        }
     }
 }
