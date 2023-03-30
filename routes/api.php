c8564eee8ed8474275b2b0a2b65a5e49a607714c
Branko Dragovic <braankoo@gmail.com>
2023-03-30 11:24:45 +0200

    Admin User routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Admin\RegisterController;
-
+use App\Http\Controllers\Admin\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,15 @@ Route::prefix('v1')->group(function () {
         Route::post('login', [LoginController::class, 'login'])->name('login');
         Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
+        Route::middleware(['jwt'])->name('user')->group(function () {
+            Route::get('user-listing', [UserController::class, 'index'])
+                ->name('index');
 
+            Route::put('user-listing/{user}', [UserController::class, 'update'])
+                ->name('update');
+
+            Route::delete('user-listing/{user}', [UserController::class, 'destroy'])
+                ->name('delete');
+        });
     })->name('admin.');
 });
