2c6d776beb185576700753254e3b28e604ad3fc7
Branko Dragovic <braankoo@gmail.com>
2023-03-30 10:27:17 +0200

    User Factory and User Seeders

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => fake()->uuid(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'is_admin' => 0,
+            'email' => fake()->safeEmail(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
+            'avatar' => null,
+            'address' => fake()->address(),
+            'phone_number' => fake()->phoneNumber(),
+            'is_marketing' => 1
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function isAdmin(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'is_admin' => true,
+        ]);
+    }
+}
