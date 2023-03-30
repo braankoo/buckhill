ca3ab2271bce8658b2cacb257cc2464ee739dc3b
Branko Dragovic <braankoo@gmail.com>
2023-03-30 11:06:28 +0200

    Admin create request

---
+++ app/Http/Requests/Admin/CreateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+final class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => ['required', new Password(8)],
+            'address' => 'required',
+            'phone_number' => 'required',
+        ];
+    }
+}
