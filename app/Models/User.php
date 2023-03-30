a29e61fc619e38a5c5cac9af463fdcb2c2bc498d
Branko Dragovic <braankoo@gmail.com>
2023-03-30 10:33:03 +0200

    HasMany reation between token and user. imported Uuuid helper to generate uuid on creating model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Helpers\UuidHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -55,4 +57,19 @@ final class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
+    public function tokens(): HasMany
+    {
+        return $this->hasMany(JwtToken::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        UuidHelper::boot(new static());
+    }
 }
