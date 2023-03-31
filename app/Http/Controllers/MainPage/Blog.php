4d9c1fd597ae39c03140e6e85f4063bb18b14d4c
Branko Dragovic <braankoo@gmail.com>
2023-03-31 16:52:22 +0200

    main page comtrollers

---
+++ app/Http/Controllers/MainPage/Blog.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\MainPage;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use App\Services\Paginator;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
+use OpenApi\Annotations as OA;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+class Blog extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/v1/main/blog",
+     *     tags={"MainPage"},
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         description="Page number",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="limit",
+     *         in="query",
+     *         description="Number of items per page",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sortBy",
+     *         in="query",
+     *         description="Number of items per page",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *      @OA\Parameter(
+     *         name="desc",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="boolean"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
+     *     )
+     * )
+     */
+    public function index(Request $request, Paginator $paginator): LengthAwarePaginator
+    {
+        $data = $paginator->paginateRequest($request, \App\Models\Post::query());
+        $data->getCollection()->transform(function ($value) {
+            $value->metadata = json_decode($value->metadata, true);
+            return $value;
+        });
+        return $data;
+    }
+
+    /**
+     *
+     *
+     * @OA\Get(
+     *     path="/api/v1/main/blog/{uuid}",
+     *     tags={"MainPage"},
+     *     @OA\Parameter(
+     *         name="uuid",
+     *         in="path",
+     *         description="UUID parameter",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *    @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
+     *     )
+     * )
+     * )
+     */
+    public function single(Post $post)
+    {
+        return Response::api(HttpResponse::HTTP_OK, 1, new PostResource($post));
+    }
+}
---
+++ app/Http/Controllers/MainPage/Promotion.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\MainPage;
+
+use App\Http\Controllers\Controller;
+use App\Services\Paginator;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use OpenApi\Annotations as OA;
+
+class Promotion extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/v1/main/promotions",
+     *     tags={"MainPage"},
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         description="Page number",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="limit",
+     *         in="query",
+     *         description="Number of items per page",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sortBy",
+     *         in="query",
+     *         description="Number of items per page",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *      @OA\Parameter(
+     *         name="desc",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="boolean"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
+     *     )
+     * )
+     */
+    public function index(Request $request, Paginator $paginator): LengthAwarePaginator
+    {
+        $data = $paginator->paginateRequest($request, \App\Models\Promotion::query());
+        $data->getCollection()->transform(function ($value) {
+            $value->metadata = json_decode($value->metadata, true);
+            return $value;
+        });
+        return $data;
+    }
+}
