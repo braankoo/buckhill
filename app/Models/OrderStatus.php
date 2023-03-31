94b9da5f1661fd0418718f798dbbbc8d2f7a3d42
Branko Dragovic <braankoo@gmail.com>
2023-03-31 23:42:41 +0200

    Order Status Model

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Helpers\UuidHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class OrderStatus extends Model
 {
     use HasFactory;
+
+
+
+    protected $fillable = ['uuid', 'title'];
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        UuidHelper::boot(new OrderStatus());
+    }
 }
