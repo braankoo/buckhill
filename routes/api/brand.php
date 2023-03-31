e1e3a43f42a5a93400003ff6cd417be0ac1b672d
Branko Dragovic <braankoo@gmail.com>
2023-03-31 23:44:14 +0200

    brand route

--- routes/api/brand.php
+++ routes/api/brand.php
@@ -7,5 +7,5 @@ Route::get('brand/{brand}', [BrandController::class, 'show']);
 Route::middleware('jwt')->group(function () {
     Route::post('brand', [BrandController::class, 'store']);
     Route::put('brand/{brand}', [BrandController::class, 'update']);
-    Route::delete('brand/{brand}', [BrandController::class, 'store']);
+    Route::delete('brand/{brand}', [BrandController::class, 'destroy']);
 });
