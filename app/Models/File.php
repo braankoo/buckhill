edf704ab747bc82634cb584d6a7cb69e0aa39804
Branko Dragovic <braankoo@gmail.com>
2023-03-31 23:40:42 +0200

    File model reoute key and fillable

--- app/Models/File.php
+++ app/Models/File.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 final class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['uuid', 'name', 'path', 'size', 'type'];
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
 }
