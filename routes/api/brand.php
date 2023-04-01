2efb5f14c510e44747334f1709872e843b303cb2
Branko Dragovic <braankoo@gmail.com>
2023-04-02 00:13:20 +0200

    Brand route naming

--- routes/api/brand.php
+++ routes/api/brand.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\BrandController;
 
-Route::get('brand', [BrandController::class, 'index']);
-Route::get('brand/{brand}', [BrandController::class, 'show']);
-Route::middleware('jwt')->group(function () {
-    Route::post('brand', [BrandController::class, 'store']);
-    Route::put('brand/{brand}', [BrandController::class, 'update']);
-    Route::delete('brand/{brand}', [BrandController::class, 'destroy']);
+
+Route::name('brand.')->group(function () {
+    Route::get('brand', [BrandController::class, 'index'])->name('index');
+    Route::get('brand/{brand}', [BrandController::class, 'show'])->name('show');
+    Route::middleware('jwt')->group(function () {
+        Route::post('brand', [BrandController::class, 'store'])->name('store');
+        Route::put('brand/{brand}', [BrandController::class, 'update'])->name('update');
+        Route::delete('brand/{brand}', [BrandController::class, 'destroy'])->name('destroy');
+    });
 });
+
