3049f2a6a0086c843a38b163eb459ffa658db1bc
Branko Dragovic <braankoo@gmail.com>
2023-04-01 23:31:50 +0200

    Admin toute naming fix

--- routes/api/admin.php
+++ routes/api/admin.php
@@ -1,15 +1,15 @@
 <?php
 
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\User\AuthController;
+use App\Http\Controllers\Admin\AuthController;
 use App\Models\User;
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::post('create', [AuthController::class, 'create'])->name('create');
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::get('logout', [AuthController::class, 'logout'])->name('logout')->middleware('api');
 
-    Route::middleware(['jwt'])->name('user')->group(function () {
+    Route::middleware('jwt:admin')->name('user.')->group(function () {
         Route::get('user-listing', [UserController::class, 'index'])
             ->can('viewAny', User::class)
             ->name('index');
@@ -22,4 +22,4 @@ Route::prefix('admin')->group(function () {
             ->can('delete', User::class)
             ->name('delete');
     });
-})->name('admin.');
+});
