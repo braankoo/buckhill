efe5e9e916412039d6e1fa10502c4de7d5e8cba1
Branko Dragovic <braankoo@gmail.com>
2023-04-02 20:02:48 +0200

    Refactor Brand test

--- tests/Feature/BrandTest.php
+++ tests/Feature/BrandTest.php
@@ -7,13 +7,9 @@ use App\Models\User;
 use App\Services\TokenService;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Tests\TestCase;
 
-class BrandTest extends TestCase
+class BrandTest extends Base
 {
-    use DatabaseTransactions;
-    use DatabaseMigrations;
-
     public function test_index(): void
     {
         $response = $this->get(route('brand.index'));
@@ -23,98 +19,68 @@ class BrandTest extends TestCase
 
     public function test_post_admin_user()
     {
-        $user = User::factory()->create(['is_admin' => 1]);
-        $token = app(TokenService::class)->login($user, true);
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token->toString(),
-            'Accept' => 'application/json'
-        ])->post(route('brand.store'), [
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getAdminUser(), true)
+        )->post(route('brand.store'), [
             'title' => fake()->word()
-        ]);
-
-        $response->assertStatus(401);
+        ])->assertStatus(401);
     }
 
     public function test_show_admin_user()
     {
-        $user = User::factory()->create(['is_admin' => 1]);
-        $token = app(TokenService::class)->login($user, true);
         $brand = Brand::factory()->create();
 
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token->toString(),
-            'Accept' => 'application/json'
-        ])->get(route('brand.show', ['brand' => $brand]));
-
-        $response->assertStatus(200);
-        $response->assertJson(['data' => ['uuid' => true]]);
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getAdminUser(), true)
+        )->get(route('brand.show', ['brand' => $brand]))
+            ->assertStatus(200)
+            ->assertJson(['data' => ['uuid' => true]]);
     }
 
     public function test_post_regular_user()
     {
-        $user = User::factory()->create(['is_admin' => 0]);
-        $token = app(TokenService::class)->login($user, true);
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token->toString(),
-            'Accept' => 'application/json'
-        ])->post(route('brand.store'), [
-            'title' => fake()->word()
-        ])->assertJson(['data' => ['uuid' => true]]);
-
-        $response->assertStatus(200);
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getRegularUser(), true)
+        )->post(
+            route('brand.store'),
+            [
+                'title' => fake()->word()
+            ]
+        )->assertJson(['data' => ['uuid' => true]])
+            ->assertStatus(200);
     }
 
     public function test_put_admin_user()
     {
-        $user = User::factory()->create(['is_admin' => 1]);
         $brand = Brand::factory()->create();
-        $token = app(TokenService::class)->login($user, true);
 
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token->toString(),
-            'Accept' => 'application/json'
-        ])->put(route('brand.update', $brand->uuid), [
-        ]);
-
-        $response->assertStatus(401);
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getAdminUser(), true)
+        )->put(route('brand.update', $brand->uuid), [
+        ])->assertStatus(401);
     }
 
     public function test_put_regular_user()
     {
-        $user = User::factory()->create(['is_admin' => 0]);
         $brand = Brand::factory()->create();
-        $token = app(TokenService::class)->login($user, true);
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token->toString(),
-            'Accept' => 'application/json'
-        ])->put(
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getRegularUser(), true)
+        )->put(
             route('brand.update', $brand->uuid),
             [
                 'title' => fake()->word()
             ]
-        );
-
-        $response->assertStatus(200);
+        )->assertStatus(200);
     }
 
     public function test_delete_regular_user()
     {
-        $user = User::factory()->create(['is_admin' => 0]);
         $brand = Brand::factory()->create();
-        $token = app(TokenService::class)->login($user, true);
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token->toString(),
-            'Accept' => 'application/json'
-        ])->delete(
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getRegularUser(), true)
+        )->delete(
             route('brand.destroy', $brand->uuid)
-        );
-
-
-        $response->assertStatus(200);
+        )->assertStatus(200);
         self::assertEquals(Brand::find($brand)->count(), 0);
     }
 }
