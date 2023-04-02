28981cef60483bf5ae8e3cf665d9e015672b559d
Branko Dragovic <braankoo@gmail.com>
2023-04-02 20:17:18 +0200

    Order Tests Refactored

---
+++ tests/Feature/OrderTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Order;
+use App\Models\OrderStatus;
+use App\Models\Payment;
+use App\Models\Product;
+use App\Services\TokenService;
+
+
+class OrderTest extends Base
+{
+
+    public function test_index(): void
+    {
+        $response = $this->get(route('order.index'));
+
+        $response->assertStatus(401);
+    }
+
+    public function test_index_admin_user(): void
+    {
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getAdminUser(), true)
+        )->get(route('order.index'))
+            ->assertStatus(401);
+    }
+
+    public function test_index_regular_user(): void
+    {
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($this->getRegularUser(), true)
+        )->get(route('order.index'))
+            ->assertStatus(200);
+    }
+
+    public function test_show_single()
+    {
+        $user = $this->getRegularUser();
+        $order = Order::factory()->complete()->create(['user_id' => $user->id]);
+
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($user, true)
+        )->get(route('order.show', ['order' => $order->uuid]))
+            ->assertStatus(200);
+    }
+
+    public function test_put_admin_user()
+    {
+        $user = $this->getAdminUser();
+        $order = Order::factory()->complete()->create(['user_id' => $user->id]);
+        $this->httpRequestWithToken(
+            app(TokenService::class)->login($user, true)
+        )->put(
+            route(
+                'order.update',
+                [
+                    'order' => $order->uuid
+                ]
+            ),
+            ['order' => $order->uuid]
+        )->assertStatus(401);
+    }
+
+    public function test_put_regular_user()
+    {
+        $user = $this->getRegularUser();
+        $payment = Payment::factory()->create();
+        $oderStatus = OrderStatus::factory()->create();
+        $order = Order::factory()->complete()->create(['user_id' => $user->id]);
+
+        $response = $this->httpRequestWithToken(app(TokenService::class)->login($user, true))->put(
+            route(
+                'order.update',
+                [
+                    'order' => $order->uuid
+                ]
+            ),
+            [
+                'order_status_uuid' => $oderStatus->uuid,
+                'payment_uuid' => $payment->uuid,
+                'amount' => '123123123',
+                'products' => json_encode(
+                    ['product' => Product::factory()->complete()->create()->uudi, 'quantity' => rand(1, 20)]
+                ),
+                'address' => [
+                    'billing' => '123',
+                    'shipping' => '123'
+                ]
+            ]
+        );
+        $response->assertStatus(200);
+        self::assertEquals(Order::where('id', '=', $order->id)->first()->amount, '123123123');
+    }
+
+    public function test_delete()
+    {
+        $user = $this->getRegularUser();
+        $order = Order::factory()->complete()->create(['user_id' => $user->id]);
+        $response = $this->httpRequestWithToken(
+            app(TokenService::class)->login($user, true)
+        )->delete(
+            route(
+                'order.destroy',
+                [
+                    'order' => $order->uuid
+                ]
+            )
+        );
+        $response->assertStatus(200);
+        self::assertEquals(Order::where('id', '=', $order->id)->count(), 0);
+    }
+
+    public function test_download()
+    {
+        $user = $this->getRegularUser();
+        $order = Order::factory()->complete()->create(['user_id' => $user->id]);
+        $response = $this->httpRequestWithToken(
+            app(TokenService::class)->login($user, true)
+        )->get(
+            route(
+                'orders.download',
+                [
+                    'order' => $order->uuid
+                ]
+            )
+        );
+        $response->assertStatus(200);
+        $response->assertHeader('Content-Type', 'application/pdf');
+    }
+
+}
