1204c6b285a62d490cef1ffbc60d0bc0baf9c203
Branko Dragovic <braankoo@gmail.com>
2023-04-02 17:14:28 +0200

    Main Page Tests

---
+++ tests/Feature/MainPageTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MainPageTest extends TestCase
+{
+
+    use DatabaseTransactions;
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_index(): void
+    {
+        $response = $this->get(route('main.promotion.index'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_blog_index(): void
+    {
+        $response = $this->get(route('main.blog.index'));
+
+
+        $response->assertStatus(200);
+    }
+
+    public function test_blog_single(): void
+    {
+        $post = Post::factory()->create();
+        $response = $this->get(route('main.blog.single', ['post' => $post->uuid]));
+
+
+        $response->assertStatus(200);
+    }
+}
